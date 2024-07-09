0c8b01e8b3b9b78680b093b70e61ba75c94f74a2
xavys1619 <jchimbolemac5@unemi.edu.ec>
2024-07-08 21:14:46 -0500

    Update urls.py

--- eventos_persona/urls.py
+++ eventos_persona/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for eventos_persona project.
+URL configuration for ManageEventos project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from eventos.views import CustomLoginView, CustomLogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('eventos.urls')),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
